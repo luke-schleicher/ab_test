ca8dd414c1169a10b81c87d00e6bc9288ae97824
luke-schleicher <luke.j.schleicher@gmail.com>
2017-03-30 20:44:59 -0500

    create employees auth - create employees controller - create nav and root

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :employees
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :employees
+
+  root 'employees#index'
+
 end
